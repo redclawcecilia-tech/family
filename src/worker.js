8c75200124b937c85452dc1e220bbd88083e25cb
family-nav-monitor <monitor@localhost>
2026-05-31 13:05:48 +0800

    Revert "Use minimal headers for family origin proxy"
    This reverts commit 89e411fdb8cbb70f2b2fa6be411cd327ebc0e560.

--- src/worker.js
+++ src/worker.js
@@ -5,11 +5,10 @@ export default {
     const incomingUrl = new URL(request.url);
     const originUrl = new URL(incomingUrl.pathname + incomingUrl.search, ORIGIN);
 
-    const headers = new Headers();
-    headers.set("Accept", request.headers.get("Accept") || "*/*");
-    headers.set("User-Agent", "family-cloudflare-proxy");
-    const contentType = request.headers.get("Content-Type");
-    if (contentType) headers.set("Content-Type", contentType);
+    const headers = new Headers(request.headers);
+    headers.set("Host", originUrl.host);
+    headers.set("X-Forwarded-Host", incomingUrl.host);
+    headers.set("X-Forwarded-Proto", incomingUrl.protocol.replace(":", ""));
 
     const originRequest = new Request(originUrl, {
       method: request.method,
@@ -30,8 +29,8 @@ export default {
     responseHeaders.set("X-Family-Proxy", "cloudflare-worker");
     responseHeaders.delete("Content-Length");
 
-    const responseContentType = responseHeaders.get("Content-Type") || "";
-    if (responseContentType.includes("text/html")) {
+    const contentType = responseHeaders.get("Content-Type") || "";
+    if (contentType.includes("text/html")) {
       const html = await originResponse.text();
       const rewritten = html.replace(
         /apiBase:\s*["']http:\/\/118\.196\.102\.49:8080["']/,
